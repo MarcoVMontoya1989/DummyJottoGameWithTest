360cd9e932b3653a9b5f434275c535b7ee009322
Marco Antonio V Montoya <marcovmontoya@gmail.com>
2020-07-19 22:56:13 -0400

    feat: updated app index

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import CongratsComponent from "./components/Congrats.component";
-import './App.css';
+import GuessedWordComponent from "./components/component-test/GuessedWord.component";
 
-function App() {
-  return (
-    <div className="App">
-      <CongratsComponent success={false}/>
-    </div>
-  );
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <CongratsComponent success={false}/>
+        <GuessedWordComponent guessedWords={[]} />
+      </div>
+    );
+  }
 }
 
 export default App;
